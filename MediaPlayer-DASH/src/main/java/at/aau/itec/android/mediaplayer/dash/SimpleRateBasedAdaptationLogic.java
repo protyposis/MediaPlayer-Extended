d32b44c660b0b95b6b4c7a9f94b60957e4e3596a
Mario Guggenberger <mg@protyposis.net>
2014-09-25 09:16:08 +0200

    rate based adaptation fixed and improved: start with lowest bitrate, not highest; statefulness regarding the adaptation sets; downswitch instantly, upswitch after 10 secs of higher bandwith

--- MediaPlayer-DASH/src/main/java/at/aau/itec/android/mediaplayer/dash/SimpleRateBasedAdaptationLogic.java
+++ MediaPlayer-DASH/src/main/java/at/aau/itec/android/mediaplayer/dash/SimpleRateBasedAdaptationLogic.java
@@ -21,6 +21,11 @@ package at.aau.itec.android.mediaplayer.dash;
 
 import android.util.Log;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Mario on 05.09.2014.
  */
@@ -28,22 +33,47 @@ public class SimpleRateBasedAdaptationLogic implements AdaptationLogic {
 
     private static final String TAG = SimpleRateBasedAdaptationLogic.class.getSimpleName();
 
+    /* The running bandwidth average can be the same for all adaptation sets since they all
+     * download their segments from the same network. */
     private RunningAverage mRunningAverage;
+
+    private Map<AdaptationSet, AdaptationState> mStateMap;
 
     public SimpleRateBasedAdaptationLogic() {
         mRunningAverage = new RunningAverage(10);
+        mStateMap = new HashMap<AdaptationSet, AdaptationState>();
+    }
+
+    private AdaptationState getState(AdaptationSet adaptationSet) {
+        AdaptationState state = mStateMap.get(adaptationSet);
+        if(state == null) {
+            state = new AdaptationState();
+            mStateMap.put(adaptationSet, state);
+        }
+        return state;
     }
 
     @Override
     public Representation initialize(AdaptationSet adaptationSet) {
+        // sort representations by bandwidth ascending
+        Collections.sort(adaptationSet.representations, new Comparator<Representation>() {
+            @Override
+            public int compare(Representation lhs, Representation rhs) {
+                return lhs.bandwidth - rhs.bandwidth;
+            }
+        });
+
         return calculateRepresentation(adaptationSet);
     }
 
     @Override
-    public void reportSegmentDownload(AdaptationSet adaptationSet, Representation representation, Segment segment, int byteSize, long downloadTimeMs) {
+    public void reportSegmentDownload(AdaptationSet adaptationSet, Representation representation,
+                                      Segment segment, int byteSize, long downloadTimeMs) {
         int bandwidth = (int)(byteSize * 8 / (downloadTimeMs / 1000f));
         int averageBandwidth = mRunningAverage.next(bandwidth);
-        Log.d(TAG, bandwidth + "bps current, " + averageBandwidth + " bps average");
+        Log.d(TAG, adaptationSet.getGroup() + " "
+                + bandwidth + "bps current, "
+                + averageBandwidth + " bps average");
     }
 
     @Override
@@ -58,8 +88,8 @@ public class SimpleRateBasedAdaptationLogic implements AdaptationLogic {
 
         /* Under the assumption that the representations are always ordered by ascending bandwidth
          * in an MPD, the representation is solely chosen upon the index.
-         * TODO order by bitrate to make sure
          */
+        AdaptationState state = getState(adaptationSet);
         int averageBandwidth = mRunningAverage.average();
         Representation newRepresentation = null;
         for(Representation representation : adaptationSet.representations) {
@@ -69,17 +99,49 @@ public class SimpleRateBasedAdaptationLogic implements AdaptationLogic {
                 break;
             }
         }
+
         if(newRepresentation == null) {
             /* When all representations require more bandwidth than currently available,
              * the lowest representation is selected.
              */
-            newRepresentation = adaptationSet.representations.get(adaptationSet.representations.size()-1);
+            newRepresentation = adaptationSet.representations.get(0);
+        }
+
+        if(state.currentRepresentation == null) {
+            /* At the first calculation, the current representation is null and gets set to the
+             * determined representation.
+             */
+            state.currentRepresentation = newRepresentation;
+        } else {
+            if(newRepresentation.bandwidth < state.currentRepresentation.bandwidth) {
+                state.vote = -1;
+            } else if(newRepresentation.bandwidth > state.currentRepresentation.bandwidth) {
+                state.vote++;
+            }
+
+            /* If the vote is below zero, for what a singe downvote suffices, the adaptation
+             * switches down to the fitting lower bandwidth representation.
+             * If the there have been consecutive upvotes for at least 10 seconds in a row, the
+             * adaptation switches to the fitting higher bandwidth representation.
+             * Any other case does not make a change and the current representation is kept. */
+            if(state.vote < 0 || state.vote >= Math.max(1, 10000000d / state.currentRepresentation.segmentDurationUs)) {
+                Log.d(TAG, "vote=" + state.vote + " switch");
+                state.currentRepresentation = newRepresentation;
+                state.vote = 0;
+            } else {
+                newRepresentation = state.currentRepresentation;
+            }
         }
 
         return newRepresentation;
     }
 
-    public static class RunningAverage {
+    private static class AdaptationState {
+        private Representation currentRepresentation;
+        private int vote;
+    }
+
+    private static class RunningAverage {
 
         private int[] values;
         private int fillLevel;
