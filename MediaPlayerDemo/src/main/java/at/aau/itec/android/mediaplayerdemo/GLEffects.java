protyposis/MediaPlayer-Extended

commit 83da470fc286b43596b1a4856f3b69b45cf043a8

detect GPU support of floating point framebuffer texture attachments if unsupported, fallback to byte textures and disable flowabs (which requires it) to avoid crashes

MediaPlayerDemo/src/main/java/at/aau/itec/android/mediaplayerdemo/GLEffects.java
@@ -25,11 +25,13 @@ import android.view.MenuItem;
 import android.view.SubMenu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import at.aau.itec.android.mediaplayer.GLTextureView;
+import at.aau.itec.android.mediaplayer.effects.FlowAbsSubEffect;
 import at.aau.itec.android.mediaplayer.effects.QrMarkerEffect;
 import at.aau.itec.android.mediaplayer.effects.Effect;
 import at.aau.itec.android.mediaplayer.effects.FlipEffect;
@@ -42,6 +44,7 @@ import at.aau.itec.android.mediaplayer.effects.KernelSharpenEffect;
 import at.aau.itec.android.mediaplayer.effects.NoEffect;
 import at.aau.itec.android.mediaplayer.effects.SimpleToonEffect;
 import at.aau.itec.android.mediaplayer.effects.SobelEffect;
+import at.aau.itec.android.mediaplayer.gles.GLUtils;
 import at.aau.itec.android.mediaplayerdemo.testeffect.ColorFilterEffect;
 
 /**
@@ -111,8 +114,13 @@ public class GLEffects implements GLTextureView.OnEffectInitializedListener {
     }
 
     public void selectEffect(int index) {
-        mSelectedEffect = mEffects.get(index);
-        mGLView.selectEffect(index);
+        Effect effect = mEffects.get(index);
+        if(!GLUtils.HAS_FLOAT_FRAMEBUFFER_SUPPORT && (effect instanceof FlowAbsEffect || effect instanceof FlowAbsSubEffect)) {
+            Toast.makeText(mActivity, "FlowAbs deactivated (GPU does not support fp framebuffer attachments)", Toast.LENGTH_SHORT).show();
+        } else {
+            mSelectedEffect = effect;
+            mGLView.selectEffect(index);
+        }
     }
 
     public Effect getSelectedEffect() {

MediaPlayer-GLES-FlowAbs/src/main/java/at/aau/itec/android/mediaplayer/effects/FlowAbsSubEffect.java
@@ -25,9 +25,16 @@ package at.aau.itec.android.mediaplayer.effects;
  * Used to share a common FlowAbs instance across all FlowAbs subeffects. Initializing one FlowAbs
  * instance per effect takes too much time and memory.
  */
-abstract class FlowAbsSubEffect extends BaseEffect {
+public abstract class FlowAbsSubEffect extends BaseEffect {
 
     protected FlowAbsEffect mFlowAbsEffect;
+
+    FlowAbsSubEffect(String name) {
+        super(name);
+    }
+
+    FlowAbsSubEffect() {
+    }
 
     @Override
     public void init(int width, int height) {

MediaPlayer-GLES/src/main/java/at/aau/itec/android/mediaplayer/gles/GLUtils.java
@@ -45,6 +45,7 @@ public class GLUtils {
     public static boolean HAS_GLES30;
     public static boolean HAS_GL_OES_texture_half_float;
     public static boolean HAS_GL_OES_texture_float;
+    public static boolean HAS_FLOAT_FRAMEBUFFER_SUPPORT;
 
     /**
      * Sets the static feature flags. Needs to be called from a GLES context.
@@ -53,6 +54,20 @@ public class GLUtils {
         HAS_GLES30 = Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2;
         HAS_GL_OES_texture_half_float = checkExtension("GL_OES_texture_half_float");
         HAS_GL_OES_texture_float = checkExtension("GL_OES_texture_float");
+
+        /* Try to create a framebuffer with an attached floating point texture. If this fails,
+         * the device does not support floating point FB attachments and needs to fall back to
+         * byte textures ... and possibly deactivate features that demand FP textures.
+         */
+        try {
+            // must be set to true before the check, otherwise the fallback kicks in
+            HAS_FLOAT_FRAMEBUFFER_SUPPORT = true;
+            new Framebuffer(8, 8);
+        } catch (RuntimeException e) {
+            Log.w(TAG, "float framebuffer test failed");
+            HAS_FLOAT_FRAMEBUFFER_SUPPORT = false;
+            GLUtils.clearError();
+        }
     }
 
     /**
@@ -64,7 +79,7 @@ public class GLUtils {
         return configurationInfo != null && configurationInfo.reqGlEsVersion >= 0x20000;
     }
 
-    public static void checkError(String operation) {
+    private static void checkError(String operation, boolean throwException) {
         int errorCount = 0;
         int error;
         String msg = null;
@@ -75,9 +90,17 @@ public class GLUtils {
             errorCount++;
         }
 
-        if(errorCount > 0) {
+        if(throwException && errorCount > 0) {
             throw new RuntimeException(msg);
         }
+    }
+
+    public static void checkError(String operation) {
+        checkError(operation, true);
+    }
+
+    public static void clearError() {
+        checkError("error clearance", false);
     }
 
     public static String[] getExtensions() {

MediaPlayer-GLES/src/main/java/at/aau/itec/android/mediaplayer/gles/Texture2D.java
@@ -82,7 +82,7 @@ public class Texture2D extends Texture {
     }
 
     public static Texture2D generateFloatTexture(int width, int height) {
-        if(GLUtils.HAS_GLES30 && GLUtils.HAS_GL_OES_texture_half_float) {
+        if(GLUtils.HAS_GLES30 && GLUtils.HAS_GL_OES_texture_half_float && GLUtils.HAS_FLOAT_FRAMEBUFFER_SUPPORT) {
             return new Texture2D(GLES30.GL_RGBA16F, GLES20.GL_RGBA, width, height, GLES20.GL_FLOAT, null);
         } else {
             Log.i(TAG, "Texture fallback mode to GLES20 8 bit");

commit 7b7a844bd094dc7e56085d2a4435539db34b7b56

Fixed bug introduced in commit 83da470 A flowabs effect selection led to loading of the parameter panel even when flowabs is unsupported on the device, leading to a NPE

MediaPlayerDemo/src/main/java/at/aau/itec/android/mediaplayerdemo/GLEffects.java
@@ -113,13 +113,15 @@ public class GLEffects implements GLTextureView.OnEffectInitializedListener {
         return effectNames;
     }
 
-    public void selectEffect(int index) {
+    public boolean selectEffect(int index) {
         Effect effect = mEffects.get(index);
         if(!GLUtils.HAS_FLOAT_FRAMEBUFFER_SUPPORT && (effect instanceof FlowAbsEffect || effect instanceof FlowAbsSubEffect)) {
             Toast.makeText(mActivity, "FlowAbs deactivated (GPU does not support fp framebuffer attachments)", Toast.LENGTH_SHORT).show();
+            return false;
         } else {
             mSelectedEffect = effect;
             mGLView.selectEffect(index);
+            return true;
         }
     }
 
@@ -138,8 +140,7 @@ public class GLEffects implements GLTextureView.OnEffectInitializedListener {
 
     private boolean doMenuActionEffect(MenuItem item) {
         if(item.getGroupId() == R.id.action_list_effects) {
-            selectEffect(item.getItemId());
-            return true;
+            return selectEffect(item.getItemId());
         }
         return false;
     }