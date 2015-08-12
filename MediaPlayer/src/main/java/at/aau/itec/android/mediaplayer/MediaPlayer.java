36a462348e8632065df6764bbd95f3aecf594bd0
Mario Guggenberger <mg@protyposis.net>
2015-08-12 18:27:21 +0200

    Fix NPE from checking wrong variable #2
    The previous fix in 422a7ab fixed the case when a separate audio stream cannot be decoded, but crashed the case when a multiplexed stream is available. This improvement now hopefully fixes all cases.

--- MediaPlayer/src/main/java/at/aau/itec/android/mediaplayer/MediaPlayer.java
+++ MediaPlayer/src/main/java/at/aau/itec/android/mediaplayer/MediaPlayer.java
@@ -699,7 +699,7 @@ public class MediaPlayer {
 
                         mVideoCodec.releaseOutputBuffer(outputBufIndex, render); // render picture
 
-                        if (mAudioFormat != null && !mSeeking && !mPaused) {
+                        if (mAudioFormat != null & mAudioExtractor != null && !mSeeking && !mPaused) {
                             // TODO rewrite; this is just a quick and dirty hack
                             long start = SystemClock.elapsedRealtime();
                             while (mAudioPlayback.getBufferTimeUs() < 100000) {
