7d98c4f6a79d35c684ce6a5fb8210b7cd1bdf4b7
Mario Guggenberger <mg@protyposis.net>
2015-08-13 12:20:51 +0200

    Simplify timebase initialization

--- MediaPlayer/src/main/java/at/aau/itec/android/mediaplayer/MediaPlayer.java
+++ MediaPlayer/src/main/java/at/aau/itec/android/mediaplayer/MediaPlayer.java
@@ -403,11 +403,6 @@ public class MediaPlayer {
         /* Flag notifying that the decoder has changed to a new representation, post-actions need to
          * be carried out. */
         private boolean mRepresentationChanged;
-        /* Flag needed to initialize the time base with the first PTS of the first frame after it
-         * has been decoded. This is necessary because the first PTS does not necessarily need to be
-         * zero. This is especially important for live streams where playback can start at an
-         * arbitrary position into the stream. */
-        private boolean mTimeBaseInitialized;
 
         private PlaybackThread() {
             super(TAG);
@@ -464,6 +459,9 @@ public class MediaPlayer {
                 boolean preparing = true; // used on startup to process stream until the first frame
                 int frameSkipCount = 0;
                 long lastPTS = 0;
+
+                // Initialize the time base with the first PTS that must not necessarily be 0
+                mTimeBase.startAt(mVideoMinPTS);
 
                 while (!mVideoOutputEos) {
                     if (mPaused && !mSeekPrepare && !mSeeking && !preparing) {
@@ -599,7 +597,7 @@ public class MediaPlayer {
                                      * EXACT mode is desired, we need to take the previous frame's PTS
                                      * and repeat the seek with that PTS to arrive at the desired frame.
                                      */
-                                    Log.d(TAG, "exact seek: repeat seek for previous frame");
+                                    Log.d(TAG, "exact seek: repeat seek for previous frame at " + lastPTS);
                                     render = false;
                                     seekTo(lastPTS);
                                 } else {
@@ -624,13 +622,6 @@ public class MediaPlayer {
                                 }
                             }
                         } else {
-                            if(!mTimeBaseInitialized) {
-                                // Initialize the time base with the first PTS
-                                // This happens only once after the first frame is decoded.
-                                mTimeBaseInitialized = true;
-                                mTimeBase.startAt(mVideoInfo.presentationTimeUs);
-                            }
-
                             mCurrentPosition = mVideoInfo.presentationTimeUs;
 
                             long waitingTime = mTimeBase.getOffsetFrom(mVideoInfo.presentationTimeUs);
