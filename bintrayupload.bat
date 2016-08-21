dfed183983db8675990aa152e96d8f2215f4c5db
Mario Guggenberger <mg@protyposis.net>
2016-08-21 15:37:42 +0200

    Update bintray upload script

--- bintrayupload.bat
+++ bintrayupload.bat
@@ -5,7 +5,7 @@ REM Uploading all modules at once (with one gradle command) does not work any mo
 
 SET username=%1
 SET apikey=%2
-SET modules="MediaPlayer" "MediaPlayer-DASH" "MediaPlayer-GLES" "MediaPlayer-GLES-FlowAbs" "MediaPlayer-GLES-QrMarker"
+SET modules="MediaPlayer" "MediaPlayer-DASH"
 
 FOR %%m in (%modules%) DO (
 	gradlew %%~m:clean %%~m:build %%~m:bintrayUpload -PbintrayUser=%username% -PbintrayKey=%apikey% -PdryRun=false
